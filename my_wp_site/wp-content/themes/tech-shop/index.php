35ea509a53e88fdd35c8fc3b1da8a51568e198fd
farhanawdpf <farhanawdpf@gmail.com>
2024-05-30 18:46:32 +0600

    update

--- my_wp_site/wp-content/themes/tech-shop/index.php
+++ my_wp_site/wp-content/themes/tech-shop/index.php
@@ -1,59 +1,63 @@
-<!DOCTYPE html>
-<html lang="<?php language_attributes()?>">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head();?>
-</head>
-<body class="">
-<div> 
-<section id="sidebar">
-
-    <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-    
-    <?php dynamic_sidebar('sidebar'); ?><br>
-    
-        <?php else : ?>
-    
-        <div class="meta-default">
-
-            <?php /* optional */ 
-			the_widget( 'WP_Widget_Categories', '', '' ); ?>
-                <?php /* minimal */ 
-				get_search_form(); ?>
-
-					<nav class="sidebar-login">
-                        <ul><?php wp_loginout(); ?></ul>
-					</nav>
-        </div>
-
-    <?php endif;  ?>
-
-</section>
-</div>
-<ul id="sidebar">
-<?php if ( ! dynamic_sidebar() ) : ?>
-	<li>{static sidebar item 1}</li>
-	<li>{static sidebar item 2}</li>
-<?php endif; ?>
-</ul>
-
-<?php 
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
-        ?> 
-	
-		<?php the_title( '<h3>', '</h3>' ); ?>
-
-        <?php
-	
-	} // end while
-} // end if
+<?php
+// in index.php or where you want to include header
+get_header(); 
 ?>
+<div class="container"> 
+    <div class="row"> 
+        <div class="col-md-8"> 
+        <?php 
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post(); 
+                ?>
+                <h2>
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <?php the_content();  
+                //the_excerpt(); ?>
+                
+                <?php
+            } // end while
+            //the_excerpt() --short description
+            //the_content()--full description
+        } // end if
+        ?>
+        </div>
+        <div class="col-md-4"> 
+        <h2 style="color:aqua">sidebar</h2>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
 
 
-    <?php wp_footer();?>
-</body>
-</html>
+
+</div>
+<?php get_footer()?>
